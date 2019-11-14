e53a8fb62db355755d5ee99355ff611d2ee2084b
Teemu <teemu.palonen98@gmail.com>
2019-11-14 18:15:48 +0200

    First Wall Transparency Success

---
+++ Assets/Scripts/WallTransparency.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTransparency : MonoBehaviour
+{
+    Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(screenPos);
+        
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            if (hit.collider.tag == "Wall")
+            {
+                renderer = hit.transform.GetComponent<Renderer>();
+                renderer.enabled = false;
+            }
+            else
+            {
+                if(renderer != null)
+                {
+                    renderer.enabled = true;
+                }
+            }
+        }
+    }
+}
