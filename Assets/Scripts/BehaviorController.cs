922ca49422564d9de57d64f00183fbea22e6afcf
Mikko <mikkokyllonen1@kamk.fi>
2019-11-15 13:29:13 +0200

    Refined enemy pathfinding & added pausing
    Enemy now moves a predetermined, pseudo-random amount of nodes, after which it stops for another predetermined, pseudo-random amount, and then starts another node batch to go through

--- Assets/Scripts/BehaviorController.cs
+++ Assets/Scripts/BehaviorController.cs
@@ -8,21 +8,32 @@ public class BehaviorController : MonoBehaviour
     public float attackDistance;
     public float attackRate;
     private float nextAttack;
+    public int minNodes;
+    public int maxNodes;
+    public float minWait;
+    public float maxWait;
 
     public Transform[] points;
-    private int destPoint = 0;
-    private int prevDest = 0;
     private NavMeshAgent agent;
+    private int nodesUntilWait;
+
+
+    private int RNG = 1;
+    public Transform debugDestination;
+    public int debugNodesUntilWait;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        RNG = RandomNumber();
     }
 
     void Update()
     {
-            /*Choose the next destination point when the agent gets
-              close to the current one.*/
+        debugNodesUntilWait = nodesUntilWait + 1;
+
+        /*Choose the next destination point when the agent gets
+          close to the current one.*/
         if (!agent.pathPending && agent.remainingDistance < 0.5f)
         {
             GoToNextPoint();
@@ -31,24 +42,42 @@ public class BehaviorController : MonoBehaviour
 
     void GoToNextPoint()
     {
-
+        //If no nodes, AI doesn't move.
         if (points.Length == 0)
         {
             return;
         }
 
-        //Set the agent to go to the currently selected destination.
-        StartCoroutine(wait());
-        agent.destination = points[RandomNode()].position;
+        /*Checks if it's time to stop, then randomly selects next destination.*/
+        if (nodesUntilWait == 0)
+        {
+            StartCoroutine(Wait());
+        }
+
+        NextNode();
     }
 
-    int RandomNode()
+    int RandomNumber()
     {
         return Random.Range(0, points.Length);
     }
 
-    IEnumerator wait()
+    void NextNode()
     {
-        yield return new WaitForSeconds(10);
+        RNG = RandomNumber();
+        agent.destination = points[RNG].position;
+        debugDestination = points[RNG];
+        nodesUntilWait--;
+
+        return;
+    }
+
+    //Stops AI for a random amount, then sets the amount of nodes to pass until next wait.
+    IEnumerator Wait()
+    {
+        agent.speed = 0.0f;
+        yield return new WaitForSecondsRealtime(Random.Range(minWait, maxWait));
+        agent.speed = 3.5f;
+        nodesUntilWait = Random.Range(minNodes, maxNodes);
     }
 }
